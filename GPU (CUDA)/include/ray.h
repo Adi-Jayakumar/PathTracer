ce197720128615ce203032015dcf3dfa056a1a79
Adithya Jayakumar <adi.jayakumar@gmail.com>
2020-08-14 18:32:29 +0100

    Simple images generated

--- GPU (CUDA)/include/ray.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "vector.h"
-struct Ray
-{   
-    Vec o, d; // origin, direction
-    Ray();
-    Ray(Vec _o, Vec _d);
-    friend std::ostream &operator<<(std::ostream &out, Ray const &h);
-};
--- GPU (CUDA)/src/solid.cpp
+++
@@ -1,8 +0,0 @@
-#include "solid.h"
-Solid::Solid(std::shared_ptr<Shape> _shape, Vec _e, Vec _c, Surface _s)
-{
-    shape = _shape;
-    e = _e;
-    c = _c;
-    s = _s;
-}
